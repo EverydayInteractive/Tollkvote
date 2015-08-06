c97ebb354a8b953f2629316cf477ff2d0f0e62c6
Henrik Gundersen <henrik.gundersen@everyday.as>
2015-08-07 01:14:45 +0200

    Added more design

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    //Prevent 'bounce’ effect when scrolling past the top/bottom
+    //document.addEventListener('touchmove', function (event) {
+    //    event.preventDefault();
+    //}, false);
+
+    //Fastclick JS - Remove 300ms delay on click
+    FastClick.attach(document.body);
+
+    // Handler for popup
+    $("[data-trigger='popup']").click(function () {
+        $(".app-inner").toggleClass("popup-open");
+        $(".popup").toggleClass("open");
+        $(".navbar").toggleClass("open");
+    });
+});
