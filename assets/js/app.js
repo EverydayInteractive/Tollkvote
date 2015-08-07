32149ce83b0deb1af8b7a0540a91a285e1bfed95
Henrik Gundersen <henrik.gundersen@everyday.as>
2015-08-08 00:20:32 +0200

    Javascript Compression

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,9 @@
 ﻿$(function () {
-    //Prevent 'bounce’ effect when scrolling past the top/bottom
-    //document.addEventListener('touchmove', function (event) {
-    //    event.preventDefault();
-    //}, false);
 
     //Fastclick JS - Remove 300ms delay on click
     FastClick.attach(document.body);
 
-    // Handler for popup
+    // Handler for popup-box
     $("[data-trigger='popup']").click(function () {
         $(".app-inner").toggleClass("popup-open");
         $(".popup").toggleClass("open");
